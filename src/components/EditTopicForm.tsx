5e5bf0b12a76679dd376e200b02f291cc49c1a4d
vmaca123 <minwoo123451@naver.com>
2025-11-18 14:25:30 +0900

    crud-1

---
+++ src/components/EditTopicForm.tsx
@@ -0,0 +1,64 @@
+﻿'use client'
+import { useState } from 'react'
+import { useRouter } from 'next/navigation'
+
+export default function EditTopicForm({
+  id,
+  title,
+  description,
+}: {
+  id: string
+  title: string
+  description: string
+}) {
+  const [newTitle, setNewTitle] = useState(title)
+  const [newDescription, setNewDescription] = useState(description)
+  const router = useRouter()
+
+  async function handleSubmit(e: React.FormEvent) {
+    e.preventDefault()
+
+    try {
+      const res = await fetch(`/api/topics/${id}`, {
+        method: 'PUT',
+        headers: {
+          'Content-type': 'application/json',
+        },
+        body: JSON.stringify({ newTitle, newDescription }),
+      })
+
+      if (!res.ok) {
+        throw new Error('Failed to update topic')
+      }
+
+      router.push('/')
+      router.refresh()
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className="flex flex-col gap-3">
+      <input
+        onChange={(e) => setNewTitle(e.target.value)}
+        value={newTitle}
+        className="border border-slate-500 px-8 py-2"
+        type="text"
+        placeholder="Topic Title"
+      />
+
+      <input
+        onChange={(e) => setNewDescription(e.target.value)}
+        value={newDescription}
+        className="border border-slate-500 px-8 py-2"
+        type="text"
+        placeholder="Topic Description"
+      />
+
+      <button className="bg-green-600 font-bold text-white py-3 px-6 w-fit">
+        Update Topic
+      </button>
+    </form>
+  )
+}
